0768431d5eb69b1fa9d4f89deaed6911b9306420
Frederick Michielssen <frederick.michielssen@openanalytics.eu>
2016-03-03 17:15:34 +0100

    Changed key binding for Submit Selection to Ctrl+Enter Added license information to the source files

--- eu.openanalytics.jupyter.console/src/eu/openanalytics/jupyter/console/Activator.java
+++ eu.openanalytics.jupyter.console/src/eu/openanalytics/jupyter/console/Activator.java
@@ -1,23 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Open Analytics NV and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package eu.openanalytics.jupyter.console;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
-/**
- * The activator class controls the plug-in life cycle
- */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
 	public static final String PLUGIN_ID = "eu.openanalytics.jupyter.console"; //$NON-NLS-1$
 
-	// The shared instance
 	private static Activator plugin;
 	
-	/**
-	 * The constructor
-	 */
 	public Activator() {
+		// Default constructor
 	}
 
 	/*
--- eu.openanalytics.jupyter.wsclient/src/eu/openanalytics/jupyter/wsclient/internal/Message.java
+++ eu.openanalytics.jupyter.wsclient/src/eu/openanalytics/jupyter/wsclient/internal/Message.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Open Analytics NV and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package eu.openanalytics.jupyter.wsclient.internal;
 
 import java.util.HashMap;
